2f52f14a854ff54cd1c26490f842f363d39a0da2
LEEJ0NGWAN <lastffx@gmail.com>
2020-09-22 15:40:44 +0900

    update

--- util.py
+++ util.py
@@ -1,5 +1,10 @@
 from math import pi
+half_pi = 0.5 * pi
+quarter_pi = 0.25 * pi
+
 # example setting
+PAGINATION = 8
+
 WIDTH = 1440
 HEIGHT = 720
 w = WIDTH//2
@@ -9,17 +14,27 @@ center = {
     'y': h,
 }
 
-def getYawPitch(x, y):
+def getYawPitch(x, y, i=None, j=None):
+    constX, constY = pi, half_pi
+    yaw, pitch = 0, 0
+    if i and j:
+        if i not in range(PAGINATION)\
+        or j not in range(PAGINATION):
+            return None
+        constX = constY = 0.5 * quarter_pi
+        yaw = quarter_pi * i
+        pitch = quarter_pi * j
+
     diff = {
         'x': x-center['x'],
         'y': y-center['y'],
     }
 
-    yaw_ = (diff['x']/w) * pi
-    pitch_ = 0.5 * (diff['y']/h) * pi
+    yaw_ = (diff['x']/w) * constX + yaw
+    pitch_ = (diff['y']/h) * constY + pitch
 
     return (yaw_, pitch_)
 
 # example
-# print(getYawPitch(251,244))
+print(getYawPitch(970,392))
 
