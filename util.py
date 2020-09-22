a673f3a48e3318ff5ec49746c68518a3abda05d2
LEEJ0NGWAN <lastffx@gmail.com>
2020-09-22 15:46:01 +0900

    수정

--- util.py
+++ util.py
@@ -2,25 +2,48 @@ from math import pi
 half_pi = 0.5 * pi
 quarter_pi = 0.25 * pi
 
-# example setting
+# (0,0) ~ (7,7)
 PAGINATION = 8
 
+# 인풋 이미지 사이즈
 WIDTH = 1440
 HEIGHT = 720
+
+# 이미지의 4분의 1 크기
 w = WIDTH//2
 h = HEIGHT//2
+
+# 이미지의 중심
 center = {
     'x': w,
     'y': h,
 }
 
+def checkRange(x, y):
+    if x < 0 or y < 0\
+    or x > WIDTH or y > HEIGHT:
+        return False
+
+    return True
+
+def checkPage(i, j):
+    if i not in range(PAGINATION)\
+    or j not in range(PAGINATION):
+        return False
+
+    return True
+
 def getYawPitch(x, y, i=None, j=None):
+    if not checkRange(x,y):
+        return None
+
     constX, constY = pi, half_pi
     yaw, pitch = 0, 0
+
     if i and j:
-        if i not in range(PAGINATION)\
-        or j not in range(PAGINATION):
+        if not checkPage(i,j):
             return None
+
         constX = constY = 0.5 * quarter_pi
         yaw = quarter_pi * i
         pitch = quarter_pi * j
