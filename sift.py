ad9e4db96e278f79d66df32051373bcc66975322
LEEJ0NGWAN <lastffx@gmail.com>
2020-10-25 23:52:35 +0900

    시프트 수정

--- sift.py
+++ sift.py
@@ -1,48 +1,61 @@
 
 from PIL import Image
+import os
 import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 import subprocess
+from util import getYawPitch
 
-popen=subprocess.Popen("extract_panorama_partial.exe")
-output = popen.wait()
+def init(data_list):
 
-print(output)
+    # popen = subprocess.Popen("extract_panorama_partial.exe", shell=True)
+    # output = popen.wait()
+
+    # print(output)
+
+    call = subprocess.call([
+        "/usr/bin/open", "-W", "-n", "-a", 
+        os.getcwd()+"/extract_panorama_partial.app"])
 
 
-for cnt in range(0 , 64):
-    row = cnt/8
-    col = cnt%8
-    dir_file=str.format('./extract_panorama_partial_Data/screenShots/screen_{0}_{1}.png',row,col)
-    img1 = cv2.imread(dir_file,0) #big
-    img2 = cv2.imread('./feature/partialinput/sinput.jpg',0) #small
- 
-    sift = cv2.xfeatures2d.SIFT_create()
- 
-    kp1, des1 = sift.detectAndCompute(img1,None)
-    kp2, des2 = sift.detectAndCompute(img2,None)
- 
-    bf = cv2.BFMatcher()
-    matches = bf.knnMatch(des1,des2, k=2)
- 
-    good = []
-    for m,n in matches:
-        if m.distance < 0.4*n.distance:
-            good.append([m])
- 
-    img4 = cv2.drawMatchesKnn(img1,kp1,img2,kp2,good,None,flags=2)
- 
-    plt.imshow(img4),plt.show()
+def work(sinput='./sinput.jpg'):
 
-    img = Image.fromarray(img4, 'RGB')
-    dir_outfile=str.format('output{0}.png',cnt)
-    img.save(dir_outfile)
+    for cnt in range(0,8):
+        for cnt2 in range(0,8):
+            print(str(cnt)+" "+str(cnt2)+":================")
+            dir_file=str.format('./extract_panorama_partial.app/Contents/screenShots/screen_{0}_{1}.png',cnt,cnt2)
+            img1 = cv2.imread(dir_file,0) #big
+            img2 = cv2.imread(sinput,0) #small
 
-#크기가 너무 크면 압축해야함. 아니면 졸라게 오래 걸림
-#YAW,PITCH,FOV는 최적 구해야 함
-#feature matching output도 압축이 가능한지 확인 해봐야함
-#비교 결과
-#정답에 영향을 주는 변인 요소 : fov, 상수, geometry map(cubemap, 6-sided, panoramic), 화질
-#1) fov 90 , 상수 0.5 정확도 : 제일 잘 찾은게 특징점 3개. 특징점 1,2개는 많았고 정확히 찾았으나, 정답이 아닌 특징점도 발생함 
-#2) fov 100 , 상수 0.4 정확도 :  거의 못찾음. 제일 잘 찾은게 특징점 1개.
+            sift = cv2.xfeatures2d.SIFT_create()
+            
+            kp1, des1 = sift.detectAndCompute(img1,None)
+            kp2, des2 = sift.detectAndCompute(img2,None)
+
+            bf = cv2.BFMatcher()
+            matches = bf.knnMatch(des1,des2, k=2)
+
+            good = []
+
+            for i, (m,n) in enumerate(matches):
+                if m.distance < 0.5*n.distance:
+                    good.append([m])
+                    # img1_idx = m.queryIdx
+                    # img2_idx = m.trainIdx
+
+                    # x - columns
+                    # y - rows
+                    # Get the coordinates
+                    # print(kp1[i].pt)
+                    # print(kp2[i].pt)
+
+            for goodie in good:
+                a = goodie[0].imgIdx
+                b = goodie[0].queryIdx
+                c = goodie[0].trainIdx
+
+                # print(kp1[b].pt)
+                x, y = kp1[b].pt
+                print(getYawPitch(x,y,cnt,cnt2))
+
