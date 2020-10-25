e6bfbd12bf184eb8b3ca42a63c9ae086c24b8441
LEEJ0NGWAN <lastffx@gmail.com>
2020-10-25 23:52:53 +0900

    약간의 수정

--- s3.py
+++ s3.py
@@ -2,24 +2,31 @@ from flask import Blueprint, Response, request, jsonify
 from serialize import serialize
 from error_handler import Error, register_handler
 from downloader import downloads
+from sift import init, work
+from thread import Thread
 
 app = Blueprint('s3_api', __name__)
 register_handler(app)
 
-@app.route('/ping/', methods=['GET'])
+@app.route('/ping', methods=['GET'])
 def ping():
     return jsonify({})
 
-@app.route('/matching/', methods=['POST'])
+@app.route('/match', methods=['POST'])
 def featureMatching():
     data = request.get_json(silent=True)
-
+    print(data)
     if 'sceneFileLocation' not in data\
     or 'evidenceFileLocations' not in data:
         raise Error(status_code=400)
 
-    for key in data:
-        downloads(data[key])
+    # for key in data:
+    #     downloads(data[key])
+
+    Thread(target=init, args=(data['evidenceFileLocations'], ))
+
+    # for key in data['evidenceFileLocations']:
+    #     Thread(target=work, args=('./'+key, ))
 
     return jsonify({})
 
